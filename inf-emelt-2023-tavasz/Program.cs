9fdc0697e007a1160810ae5d43014f2020ef06a6
gergelygrosz <groszgergely09@gmail.com>
2025-05-05 15:32:26 +0200

    <inf-emelt-2023-tavasz> v0

---
+++ inf-emelt-2023-tavasz/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static readonly string PathToInput = Environment.GetEnvironmentVariable("PROJECTS") + @"\dotnet\erettsegi-megoldasok-dotnet\inf-emelt-2023-tavasz\kep.txt";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--- 2023. TAVASZ - INFOMATIKA EMELT - RGB SZÍNEK ---");
+
+        Program program = new();
+    }
+}
