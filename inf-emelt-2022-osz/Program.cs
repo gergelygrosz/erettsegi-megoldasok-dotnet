b047bc1fc854284d23f0e253857d9a4a8a0fd728
gergelygrosz <groszgergely09@gmail.com>
2025-05-06 10:11:44 +0200

    inf-emelt-2022-osz v0.0

---
+++ inf-emelt-2022-osz/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static readonly string PathToInput = Environment.GetEnvironmentVariable("PROJECTS") + @"\dotnet\erettsegi-megoldasok-dotnet\inf-emelt-2022-osz\jel.txt";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--- 2022. ŐSZ - INFOMATIKA EMELT - ÁLLATOK MOZGÁSA ---");
+
+        Program program = new();
+        program.Feladat1();
+        program.Feladat2();
+        program.Feladat4();
+        program.Feladat5();
+        program.Feladat6();
+        program.Feladat7();
+    }
+
+    void Feladat1() { }
+    void Feladat2() { }
+    void Eltelt() { }
+    void Feladat4() { }
+    void Feladat5() { }
+    void Feladat6() { }
+    void Feladat7() { }
+}
